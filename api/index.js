4b95b8b0c55fe18da8467427fe000a8e5a093d16
Putrisj <51511910+Putrisj@users.noreply.github.com>
2021-11-20 13:46:02 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'your_telegram_token';
+const token = '2100148772:AAFT8EoNtszntvXtP0PuP_hYHyCkhsCdsTEn';
 const bot = new TelegramBot(token, {polling: true});
 
 
